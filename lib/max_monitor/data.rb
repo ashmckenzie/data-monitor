ce48e4d9268e424e626939bb396ed58e9711d3a2
Ash McKenzie <ash@greenworm.com.au>
2014-01-28 11:22:52 +1100

    yay, pmessage

--- lib/max_monitor/data.rb
+++ lib/max_monitor/data.rb
@@ -4,11 +4,11 @@ module DataMonitor
   class Data
 
     def read
-      DataMonitor::Redis.connection.subscribe('max-room:humidity') do |on|
-        on.message do |key, payload|
+      DataMonitor::Redis.connection.psubscribe('max-room:*') do |on|
+        on.pmessage do |pattern, key, payload|
           payload = payload.to_f
           _, node_name, channel = key.split(':')
-          # puts "#{Time.now}: Received node_name:#{node_name}, channel:#{channel}, payload:#{payload}"
+          puts "#{Time.now}: Received node_name:#{node_name}, channel:#{channel}, payload:#{payload}"
           yield node_name, channel, payload
         end
       end
